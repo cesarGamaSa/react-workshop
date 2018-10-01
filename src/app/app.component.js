eea1d229d0bcf3520c01cf58656a78685d94e4dc
César Sá <cesar.sa@innowave.tech>
2018-10-01 17:30:26 +0100

    step5.1

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -5,10 +5,10 @@ import env from '@env';
 const App = () => {
     console.log('env', env);
     return (
-        <div className="container">                                     
-            <div className="col-12">                                    
-                <h1 className="text-center">I should be centered!</h1>  
-            </div>                                                     
+        <div className="container">
+            <div className="col-12">
+                <h1 className="text-center">I should be centered!</h1>
+            </div>
         </div> 
     )
 };
