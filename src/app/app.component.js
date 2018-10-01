7461c3fd3115732cf2c39f8ccfac99b878e7d205
César Sá <cesar.sa@innowave.tech>
2018-10-01 16:44:56 +0100

    step5

---
+++ src/app/app.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import env from '@env';
+
+const App = () => {
+    console.log('env', env);
+    return (
+        <div className="container">                                     
+            <div className="col-12">                                    
+                <h1 className="text-center">I should be centered!</h1>  
+            </div>                                                     
+        </div> 
+    )
+};
+
+export default App;
